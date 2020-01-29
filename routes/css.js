8f51e644096717cc8725d1801d269929929849ea
M-garrigan <mgarrigan99@gmail.com>
2020-01-28 21:31:29 -0500

    Split the head.pug element into head.pug + nav-pug.pug

--- routes/css.js
+++ routes/css.js
@@ -1,10 +1,13 @@
 
 
 module.exports = ({
+  'globals.css': 'public/styles/globals.css',
   'index.css': 'public/styles/index.css',
   'nav.css': 'public/styles/nav.css',
+  'nav-pug.css': 'public/styles/nav-pug.css',
   'pug.css': 'public/styles/pug.css',
   'pug-api.css': 'public/styles/pug-api.css',
+  'pug-api.css': 'public/styles/pug-engine.css',
   'prism-okaidia.css': 'node_modules/prismjs/themes/prism-okaidia.css',
   'prism-line-numbers.css': 'node_modules/prismjs/plugins/line-numbers/prism-line-numbers.css'
 });
