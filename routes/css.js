0301a976208cc4d2feafc042fe9cf4ea97e88c19
M-garrigan <mgarrigan99@gmail.com>
2020-01-29 20:17:05 -0500

    Starting elastic page

--- routes/css.js
+++ routes/css.js
@@ -4,10 +4,12 @@ module.exports = ({
   'globals.css': 'public/styles/globals.css',
   'index.css': 'public/styles/index.css',
   'nav.css': 'public/styles/nav.css',
+  'es.css': 'public/styles/es.css',
+  'nav-es.css': 'public/styles/nav-es.css',
   'nav-pug.css': 'public/styles/nav-pug.css',
   'pug.css': 'public/styles/pug.css',
   'pug-api.css': 'public/styles/pug-api.css',
-  'pug-api.css': 'public/styles/pug-engine.css',
+  'pug-engine.css': 'public/styles/pug-engine.css',
   'prism-okaidia.css': 'node_modules/prismjs/themes/prism-okaidia.css',
   'prism-line-numbers.css': 'node_modules/prismjs/plugins/line-numbers/prism-line-numbers.css'
 });
--- routes/svgs.js
+++ routes/svgs.js
@@ -1,6 +1,8 @@
 
 module.exports = ({
+  'es-color-logo.svg': 'es-color-logo.svg',
   'es-icon-nav.svg': 'es-icon-nav.svg',
+  'es-transition.svg': 'es-transition.svg',
   'pug-icon-nav.svg': 'pug-icon-nav.svg',
   'hapi-icon-nav.svg': 'hapi-icon-nav.svg',
   'redux-icon-nav.svg': 'redux-icon-nav.svg',
