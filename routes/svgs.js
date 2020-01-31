fbea99affe5021cd1eb80e4484fc15b6ad329b2d
M-garrigan <mgarrigan99@gmail.com>
2020-01-30 21:00:17 -0500

    Moving two svgs to pug mixin

--- routes/svgs.js
+++ routes/svgs.js
@@ -1,8 +1,10 @@
 
 module.exports = ({
   'es-color-logo.svg': 'es-color-logo.svg',
+  'es-color-logo-wht.svg': 'es-color-logo-wht.svg',
   'es-icon-nav.svg': 'es-icon-nav.svg',
   'es-transition.svg': 'es-transition.svg',
+  'es-transition1.svg': 'es-transition1.svg',
   'pug-icon-nav.svg': 'pug-icon-nav.svg',
   'hapi-icon-nav.svg': 'hapi-icon-nav.svg',
   'redux-icon-nav.svg': 'redux-icon-nav.svg',
